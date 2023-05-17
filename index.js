bb8a2f531f54556d4a6b4fd12841942ca9506291
Michael CORNILLON <ei.dwaps@gmail.com>
2023-05-17 14:04:21 +0200

    présentation module http server (2)

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // MODULE HTTP/S
 
 const http = require('http');
-const server = http.createServer();
 const port = process.env.PORT || 8080;
 
-server.listen(port);
-server.on('listening', () => console.log(`Listening on port ${port}`));
-
-server.on('request', (req, res) => {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.write('<h1>Bienvenue</h1>');
-  res.end();
-})
+http
+  .createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.write('<h1>Bienvenue</h1>');
+    res.end();
+  })
+  .listen(port, () => console.log(`Listening on port ${port}`));
