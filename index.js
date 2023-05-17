5796347c73fd1e7ccb2cc80e1cfde94f974be92c
Michael CORNILLON <ei.dwaps@gmail.com>
2023-05-17 17:32:14 +0200

    présentation d'express

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-// MODULE HTTP/S
+const express = require('express');
+const app = express();
+const { resolve } = require('path');
+const router = express.Router();
 
+// CONFIGURATION DE L'APP
+app.use('/static', express.static(resolve('public')));
+app.use(router);
+
+// ROUTING
+router.get('/home', (req, res) => res.sendFile(resolve('views', 'home.html')));
+router.get('/presentation', (req, res) => res.sendFile(resolve('views', 'presentation.html')));
+router.get('/contact', (req, res) => res.sendFile(resolve('views', 'contact.html')));
+
+app.get('*', (req, res) => res.redirect('/home'));
+
+
+// LANCEMENT DU SERVEUR
 const http = require('http');
-const port = process.env.PORT || 8080;
-const { readFileSync } = require('fs');
-const { extname } = require('path');
-
-const pages = {
-  '/home': readFileSync('index.html'),
-  '/presentation': readFileSync('presentation.html'),
-  '/contact': readFileSync('contact.html'),
-  '/style.css': readFileSync('style.css'),
-  '/app.js': readFileSync('app.js'),
-};
 
 http
-  .createServer((req, res) => {
-    let contentType = 'text/html; charset=utf-8';
-    
-    switch (extname(req.url)) {
-      case '.css': contentType = 'text/css'; break;
-      case '.js': contentType = 'text/javascript'; break;
-      case '.ico':
-      case '.png': contentType = 'image/*'; break;
-    }
-    
-    if (pages[req.url]) res.write(pages[req.url]);
-    else res.writeHead(301, { Location: `http://localhost:${port}/home` });
-
-    res.end();
-  })
-  .listen(port, () => console.log(`Listening on port ${port}`));
+  .createServer(app)
+  .listen(3000, () => console.log('Listening on port 3000'));
