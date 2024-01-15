15dcf2db5eafc62c00bdcdc49fef73350c96cb3b
Damien Mouchagues <damienmouchagues33@gmail.com>
2024-01-15 17:28:02 +0100

    Modifications données

--- src/models/index.jsx
+++ src/models/index.jsx
@@ -2,25 +2,25 @@ export const profiles = [
   {
     id: "001",
     photo: "https://randomuser.me/api/portraits/men/26.jpg",
-    bio: "Expert en siestologie et amateur de tacos cosmiques. Ma vie tourne autour de la question cruciale : 'Peut-on faire une sieste dans l'espace?' 🌮💤",
+    bio: "Érudit culinaire, spécialiste du kebab, amateur de raclette et expert en siestologie. En quête de l'alliance ultime entre saveurs et siestes réparatrices ! 🌮🥙🧀💤",
     name: "Arno Camoa",
   },
   {
     id: "002",
     photo: "https://randomuser.me/api/portraits/women/26.jpg",
-    bio: "Expert en siestologie et amateur de tacos cosmiques. Ma vie tourne autour de la question cruciale : 'Peut-on faire une sieste dans l'espace?' 🌮💤",
+    bio: "Érudit culinaire, spécialiste du kebab, amateur de raclette et expert en siestologie. En quête de l'alliance ultime entre saveurs et siestes réparatrices ! 🌮🥙🧀💤",
     name: "Ava S'coucher",
   },
   {
     id: "003",
     photo: "https://randomuser.me/api/portraits/men/27.jpg",
-    bio: "Expert en siestologie et amateur de tacos cosmiques. Ma vie tourne autour de la question cruciale : 'Peut-on faire une sieste dans l'espace?' 🌮💤",
+    bio: "Érudit culinaire, spécialiste du kebab, amateur de raclette et expert en siestologie. En quête de l'alliance ultime entre saveurs et siestes réparatrices ! 🌮🥙🧀💤",
     name: "Spaghetti Joe",
   },
   {
     id: "004",
     photo: "https://randomuser.me/api/portraits/men/28.jpg",
-    bio: "Expert en siestologie et amateur de tacos cosmiques. Ma vie tourne autour de la question cruciale : 'Peut-on faire une sieste dans l'espace?' 🌮💤",
+    bio: "Érudit culinaire, spécialiste du kebab, amateur de raclette et expert en siestologie. En quête de l'alliance ultime entre saveurs et siestes réparatrices ! 🌮🥙🧀💤",
     name: "Sula Cronim",
   },
 ];
