e914bfe7c5c03cedc99f8aeccb956987920be2a9
dwaps <ei.dwaps@gmail.com>
2024-01-16 13:48:31 +0100

    tp smiley: select smiley by user

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import Smiley from "./components/Smiley";
-import { smileys } from "./models";
+import { smileys as sm } from "./models";
 
 function App() {
+  const [smileys, setSmileys] = useState(sm);
+
+  function onSelectSmiley(id) {
+    const selected = smileys
+      .map((sm) => {
+        sm.selected = false;
+        return sm;
+      })
+      .find((sm) => sm.id === id);
+
+    selected.selected = true;
+    setSmileys([...smileys]);
+  }
+
   return (
     <div className="app">
       <h2>Quelle est ton humeur du jour ?</h2>
       <div className="smileys-box">
         {smileys.map((smiley) => (
-          <Smiley key={smiley.id} smiley={smiley} />
+          <Smiley
+            key={smiley.id}
+            smiley={smiley}
+            selectSmiley={onSelectSmiley}
+          />
         ))}
       </div>
     </div>
