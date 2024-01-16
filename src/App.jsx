9cfafdf7e1fbeb5d1db64282dad43aaf0e9f8f8a
dwaps <ei.dwaps@gmail.com>
2024-01-16 10:10:39 +0100

    useState ex 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import "./App.css";
-import Books from "./components/Books/Books";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="app">
-      <Books />
+      <button onClick={() => setCount(count + 1)}>Ajouter</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count - 1)}>Soustraire</button>
     </div>
   );
 }
