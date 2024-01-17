894fc28b4a3133aac0ff0c5de4c59e1e54d97b06
dwaps <ei.dwaps@gmail.com>
2024-01-17 10:38:47 +0100

    useRef: ex avec setInterval

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,42 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TestUseEffect />
+    <Test />
   </React.StrictMode>
 );
 
-function TestUseEffect() {
-  const [recipes, setRecipes] = useState([]);
-  const myRef = useRef(null);
+export function Test() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const idInterval = useRef(null);
 
-  useEffect(() => {
-    fetch("https://dummyjson.com/recipes")
-      .then((res) => res.json())
-      .then((data) => setRecipes(data.recipes));
-  }, []);
+  let secondsPassed = 0;
+  if (startTime !== null && now !== null) {
+    secondsPassed = (now - 1000) / 1000;
+  }
+
+  function onStart() {
+    setStartTime(Date.now());
+    setNow(Date.now);
+
+    clearInterval(idInterval.current);
+    idInterval.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  }
+
+  function onStop() {
+    clearInterval(idInterval.current);
+  }
 
   return (
     <>
-      <ul>
-        {recipes.map((r) => (
-          <li onClick={() => myRef.current.scrollIntoView()} key={r.id}>
-            {r.name}
-          </li>
-        ))}
-      </ul>
-      <footer ref={myRef}>Pied de page</footer>
+      <h1>Temps passé : {secondsPassed.toFixed(3)}</h1>
+      <button onClick={onStart}>Start</button>
+      <button onClick={onStop}>Stop</button>
     </>
   );
 }
