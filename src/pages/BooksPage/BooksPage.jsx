e6fa3a4d6998c15afc6fa081f2a1e9111ce29672
dwaps <ei.dwaps@gmail.com>
2024-01-17 15:19:00 +0100

    tri par nom d'auteur

--- src/pages/BooksPage/BooksPage.jsx
+++ src/pages/BooksPage/BooksPage.jsx
@@ -1,13 +1,23 @@
+import { useContext, useState } from "react";
 import { Table } from "react-bootstrap";
+import { BooksContext } from "../../utils/context";
 import Books from "./components/Books";
 
 function BooksPage() {
+  const [isAsc, setIsAsc] = useState(false);
+  const { sortBooks } = useContext(BooksContext);
+
+  function onSortByAuthor() {
+    setIsAsc(!isAsc);
+    sortBooks();
+  }
+
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
           <th>Titre</th>
-          <th>Auteur</th>
+          <th onClick={onSortByAuthor}>Auteur {isAsc ? "🔻" : "🔺"}</th>
           <th>Pays</th>
         </tr>
       </thead>
--- src/utils/context/index.jsx
+++ src/utils/context/index.jsx
@@ -1,15 +1,29 @@
-import { createContext } from "react";
-import { books } from "../../models";
+import { createContext, useState } from "react";
+import { books as bks } from "../../models";
 
 export const BooksContext = createContext();
 
 export const BooksProvider = ({ children }) => {
+  const [books, setBooks] = useState(bks);
+
+  books.sort((a, b) => {
+    const authorA = a.author.toLowerCase();
+    const authorB = b.author.toLowerCase();
+    if (authorA > authorB) return 1;
+    if (authorA < authorB) return -1;
+    return 0;
+  });
+
   function getBook(id) {
     return books.find((b) => b.id === id);
   }
 
+  function sortBooks() {
+    setBooks(books.reverse());
+  }
+
   return (
-    <BooksContext.Provider value={{ books, getBook }}>
+    <BooksContext.Provider value={{ books, getBook, sortBooks }}>
       {children}
     </BooksContext.Provider>
   );
