c9469ea58510075c046bb50a77ed82cf5668fe35
dwaps <ei.dwaps@gmail.com>
2024-01-17 10:35:37 +0100

    useRef (dom) ET useEffet (appel API)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <TestUseEffect />
   </React.StrictMode>
 );
+
+function TestUseEffect() {
+  const [recipes, setRecipes] = useState([]);
+  const myRef = useRef(null);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/recipes")
+      .then((res) => res.json())
+      .then((data) => setRecipes(data.recipes));
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {recipes.map((r) => (
+          <li onClick={() => myRef.current.scrollIntoView()} key={r.id}>
+            {r.name}
+          </li>
+        ))}
+      </ul>
+      <footer ref={myRef}>Pied de page</footer>
+    </>
+  );
+}
